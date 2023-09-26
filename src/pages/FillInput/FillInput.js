09283f031da59f786e18e25f31af62f54cd6fe0b
ThanhNguyenDat <johnkelvin3107@gmail.com>
2023-09-27 00:41:20 +0700

    update multi task

--- src/pages/FillInput/FillInput.js
+++ src/pages/FillInput/FillInput.js
@@ -1,25 +1,23 @@
-import React, { useEffect, useMemo } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import LabelStudio from "@custom/lsf/js/main";
-import tasks from "../../data/lesson_prepared.json";
-
 import "@custom/lsf/css/main.css";
 
-const LSFFillInBlank = () => {
-    const task = useMemo(() => tasks[0], []);
+import tasks from "../../data/lesson_prepared.json";
 
+const LSFFillInBlank = ({ task }) => {
     useEffect(() => {
         // Tạo một thể hiện của Label Studio trên thành phần componentDidMount
         const lsfInstance = new LabelStudio("lsf", {
             config: `
             <View>
-            <Style>
-                .lsf-labels {
-                    display: none;
-                }
-            </Style>
-            <Labels name="label" toName="text">
-                <Label value="answer" background="red"/>
-            </Labels>
+                <Style>
+                    .lsf-labels {
+                        display: none;
+                    }
+                </Style>
+                <Labels name="label" toName="text">
+                    <Label value="answer" background="red"/>
+                </Labels>
                 <FillInBlankText name="text" value="$text"/>
             </View>
             `,
@@ -35,10 +33,10 @@ const LSFFillInBlank = () => {
                 "layout:fill-in-blank:check-btn",
             ],
             task: {
-                annotations: task.annotations,
-                predictions: task.predictions,
+                annotations: task?.annotations ?? [],
+                predictions: task?.predictions ?? [],
                 id: 1,
-                data: { ...task.data },
+                data: { ...task?.data },
             },
 
             onLabelStudioLoad: function (LS) {
@@ -48,8 +46,8 @@ const LSFFillInBlank = () => {
                 LS.annotationStore.selectAnnotation(c.id);
             },
 
-            onRetryTask: function (data) {
-                console.log("retryTask: ", data);
+            onRetryTask: function (event) {
+                console.log("retryTask: ", event);
             },
 
             onCheckTask: function (data) {
@@ -61,7 +59,7 @@ const LSFFillInBlank = () => {
             // Hủy đăng ký sự kiện và dọn sạch
             lsfInstance.destroy();
         };
-    }, []);
+    }, [task]);
 
     return (
         <div style={{ padding: 24, maxWidth: 712, margin: "0px auto" }}>
@@ -71,7 +69,51 @@ const LSFFillInBlank = () => {
 };
 
 export const FillInputPage = () => {
-    return <LSFFillInBlank />;
+    const taskId = localStorage.getItem("taskId");
+
+    var task = useMemo(() => {
+        const t = tasks.filter(
+            (task) => task.id.toString() === taskId.toString()
+        );
+        if (t.length === 0) {
+            return tasks[0];
+        }
+        return t[0];
+    }, [taskId]);
+
+    return (
+        <div>
+            <div
+                style={{
+                    position: "fixed",
+                    top: "96px",
+                    left: "32px",
+                    zIndex: "100",
+                    width: "240px",
+                    display: "grid",
+                    gridTemplateColumns: "repeat(4, 1fr)",
+                }}
+            >
+                {tasks.map((_task) => (
+                    <div
+                        key={_task.id}
+                        style={{
+                            cursor: "pointer",
+                            borderRadius: "50%",
+                        }}
+                        onClick={() => {
+                            console.log("_task.id: ", _task.id);
+                            localStorage.setItem("taskId", _task.id);
+                            window.location.reload();
+                        }}
+                    >
+                        {_task.id}
+                    </div>
+                ))}
+            </div>
+            <LSFFillInBlank task={task} />
+        </div>
+    );
 };
 
 FillInputPage.path = "fillinput";
